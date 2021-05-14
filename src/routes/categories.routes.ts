73522b4cb20e6a5f546eb9f683b1029a26f3676a
Vinicius S <vinicius.soares852@gmail.com>
2021-05-14 12:01:05 -0300

    finalizando cadastro de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,34 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
+function verifyIfCategoryExists(request, response, next) {
+  const { name } = request.body;
 
-categoriesRoutes.post("/", (request, response) => {
+  const findCategory = categoriesRepository.findByName(name);
+
+  if (findCategory) {
+    return response.status(403).json({ error: "Category already exists!" });
+  }
+
+  return next();
+}
+
+categoriesRoutes.post("/", verifyIfCategoryExists, (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4(),
-    name,
-    description,
-  };
+  categoriesRepository.create({ name, description });
 
-  categories.push(category);
+  return response.status(201).send();
+});
 
-  return response.status(201).json(categories);
+categoriesRoutes.get("/", (request, response) => {
+  const list = categoriesRepository.list();
+
+  return response.status(201).json(list);
 });
 
 export { categoriesRoutes };
